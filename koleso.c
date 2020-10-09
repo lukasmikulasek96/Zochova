b5ba0643d318e02735c8fa14fa4aa5f5e9042c7e
Lukáš Mikulášek <lukas.mikulasek@softec.sk>
2020-10-09 15:31:18 +0200

    Moj pravy code commit

---
+++ koleso.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+#include<math.h>
+
+//Konstanty PI = 3.14 Palectocm je prevod z palca na metre a cmtokm je prevod z cm na km
+#define PI 3.14
+#define Palectocm 2.54
+#define cmtokm 100000
+//Vlastna funkcia
+float obvodKruhu(float priemer) {
+	float vysledok;
+	
+	vysledok = PI * (priemer * Palectocm);
+
+	return vysledok;
+}
+
+
+int main() {
+	float priemerKolesa, trasa, obvodKolesa, pocetOtacok;
+
+	printf("Zadajte priemer kolesa: ");
+	scanf("%f", &priemerKolesa);
+
+	printf("Zadajte vzdialenost v km: ");
+	scanf("%f", &trasa);
+	
+	obvodKolesa = obvodKruhu(priemerKolesa);
+
+	pocetOtacok = trasa / (obvodKolesa / cmtokm);
+
+	printf("Koleso sa na ceste otoci %f krat.", pocetOtacok);
+
+	return 0;
+}
