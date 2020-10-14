692961903184664e91ac5fba812dd65d62651122
lukasmikulasek96 <72408999+lukasmikulasek96@users.noreply.github.com>
2020-10-14 15:49:00 +0200

    Program na prevod F na C
    Novy c subor

---
+++ Stupne.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+
+int main(){
+    float Farenheit, Celzia;
+
+    scanf("%f",&Farenheit);
+
+    Celzia = 5/9*(Farenheit-32);
+
+    printf("%f Fahrenheita je %f Celsiuz\n",Farenheit,Celzia);
+
+    return 0;
+
+}
