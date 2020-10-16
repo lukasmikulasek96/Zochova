9fbc37e56d266c0e44c58f66f831c42f71326e58
Lukáš Mikulášek <lukas.mikulasek@softec.sk>
2020-10-16 07:56:00 +0200

    Pridanie programu na scitavanie 2 cisiel

---
+++ cas.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main(){
+    int a, b, c;
+    printf("Zadajte prve cislo: ");
+    scanf("%d",&a);
+    printf("Zadajte druhe cislo: ");
+    scanf("%d",&b);
+    c = a + b;
+    printf("%d",c);
+
+
+    return 0;
+}
